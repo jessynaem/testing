5656f2b55e4af35e971fabe366ffdc45f82692a1
Jessica Yuen <jeyuen@deloitte.com.au>
2025-10-23 10:30:59 +1100

    revert card js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -4,29 +4,12 @@ export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
-    const li = document.createElement('li');
+    const li = document.createElement("li");
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) {
+      if (div.children.length === 1 && div.querySelector('picture'))
         div.className = 'cards-card-image';
-      } else {
-        div.className = 'cards-card-body';
-      }
-
-      // separate buttoner from the body and put into a footer container
-      const buttonContainer = div.querySelector('.button-container');
-      if (buttonContainer) {
-        // Remove button from body
-        div.removeChild(buttonContainer);
-
-        // Create footer div if not already present
-        const footer = document.createElement('div');
-        footer.className = 'cards-card-footer';
-        li.appendChild(footer);
-
-        // Append button container to footer
-        footer.appendChild(buttonContainer);
-      }
+      else div.className = 'cards-card-body';
     });
     ul.append(li);
   });
