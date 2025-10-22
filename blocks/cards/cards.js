6731d132c5e0267e0a6701c536330d8a658c0f8f
Jessica Yuen <jeyuen@deloitte.com.au>
2025-10-23 10:35:26 +1100

    reposition button element in card block

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -9,6 +9,19 @@ export default function decorate(block) {
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
+      const buttonContainer = div.querySelector('.button-container');
+      if (buttonContainer) {
+        // Remove button from body
+        div.removeChild(buttonContainer);
+
+        // Create footer div if not already present
+        const footer = document.createElement('div');
+        footer.className = 'cards-card-footer';
+        li.appendChild(footer);
+
+        // Append button container to footer
+        footer.appendChild(buttonContainer);
+      }
     });
     ul.append(li);
   });
